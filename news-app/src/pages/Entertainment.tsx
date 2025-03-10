3651af99dcd53de0f699cc79f8c51e31c47026b1
Mdowel <150044412+Mdowel@users.noreply.github.com>
2025-03-10 15:48:38 -0400

    copy article logic to filtered topic pages

--- news-app/src/pages/Entertainment.tsx
+++ news-app/src/pages/Entertainment.tsx
@@ -5,12 +5,29 @@ import { getEntertainmentArticles } from '../api.js'
 export default function Entertainment() {
     const [articles, setArticles] = React.useState<Article[]>([])
 
+    // interface for json placeholder
+    interface Article {
+        title: string
+        body: string
+        id: number
+    }
+// interface for newsapi
+    // interface Article  {
+    //     title: string
+    //     author?: string
+    //     description?: string
+    //     content: string
+    //     publishedAt?: string
+    //     source?: {name: string}
+    //     url: string
+    //     urlToImage?: string
+    // }
 
     React.useEffect(() => {
         async function loadArticles() {
             try {
                 const data = await getEntertainmentArticles()
-                setArticles(data as Article[])
+                setArticles(data.slice(0,17) as Article[])
             } catch (err) {
                 console.log(err)
             }
@@ -20,21 +37,22 @@ export default function Entertainment() {
 
     const articleElements = articles.map(article => {
         return (
-            <div className="article-container" key={article.title}>
-                <Link 
-                    to={article.url}
+            <Link 
+                    // to={article.url}
                     className="article-inner"
-                >
+            >
+                <div className="article-container" key={article.title}>
                     <div className="article-img-container">
-                        <img src={article.urlToImage} alt="image unavailable" />                        
+                        {/* <img src={article.urlToImage} alt="image unavailable" />                         */}
+                        <img src="https://i.insider.com/67bec42be015c5d98a6af08a?width=2000&format=jpeg&auto=webp" alt="image unavailable" />                        
                     </div>
                     <div className="article-text">
                         <h2>{article.title}</h2>
                         {/* <p>{article.description}</p> */}
-                        <p>{article.body}</p>
+                        <p id="article-paragraph">{article.body.substring(0,125) + "..."} <a href={article.url}>read more</a></p>
                     </div>
-                </Link>
-            </div>
+                </div>
+            </Link>
         )
     })
 
--- news-app/src/pages/Sports.tsx
+++ news-app/src/pages/Sports.tsx
@@ -5,11 +5,28 @@ import { getSportsArticles } from '../api.js'
 export default function Sports() {
     const [articles, setArticles] = React.useState<Article[]>([])
 
+    interface Article {
+        title: string
+        body: string
+        id: number
+    }
+// interface for newsapi
+    // interface Article  {
+    //     title: string
+    //     author?: string
+    //     description?: string
+    //     content: string
+    //     publishedAt?: string
+    //     source?: {name: string}
+    //     url: string
+    //     urlToImage?: string
+    // }
+
     React.useEffect(() => {
         async function loadArticles() {
             try {
                 const data = await getSportsArticles()
-                setArticles(data as Article[])
+                setArticles(data.slice(0,17) as Article[])
             } catch (err) {
                 console.log(err)
             }
@@ -19,20 +36,23 @@ export default function Sports() {
 
     const articleElements = articles.map(article => {
         return (
-            <div className="article-container" key={article.title}>
             <Link 
-                to={article.url}
+                // to={article.url}
                 className="article-inner"
             >
-                <div className="article-img-container">
-                    <img src={article.urlToImage} alt="image unavailable" />                        
-                </div>
-                <div className="article-text">
-                    <h2>{article.title}</h2>
-                    <p>{article.description}</p>
+                <div className="article-container" key={article.title}>
+                    <div className="article-img-container">
+                        {/* <img src={article.urlToImage} alt="image unavailable" />                         */}
+                        <img src="https://i.insider.com/67bec42be015c5d98a6af08a?width=2000&format=jpeg&auto=webp" alt="image unavailable" />                        
+
+                    </div>
+                    <div className="article-text">
+                        <h2>{article.title}</h2>
+                        {/* <p>{article.description}</p> */}
+                        {<p id="article-paragraph">{article.body.substring(0,125) + "..."} <a href={article.url}>read more</a></p>}
+                    </div>
                 </div>
             </Link>
-        </div>
         )
     })
     
