dfc27f57a1f5c33e12aeea5ae42380c96bf82a40
Mdowel <150044412+Mdowel@users.noreply.github.com>
2025-03-10 15:02:40 -0400

    footer styling

--- news-app/src/components/Footer.tsx
+++ news-app/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <div className="footer">
-            <p>&copy; 2025 RapidNews</p> 
+            <p>&copy; 2025 NewsKiwi <i className="fa-solid fa-kiwi-bird"></i></p> 
         </div>
     )
 }
