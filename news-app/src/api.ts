e06d11e4cbe426876ea50bc6ebf7f52c35215418
Mdowel <150044412+Mdowel@users.noreply.github.com>
2025-05-13 15:49:42 -0400

    reconnect API

--- news-app/src/api.ts
+++ news-app/src/api.ts
@@ -1,21 +1,21 @@
 
 // //newsAPI version
-// export async function getAllArticles( category? : string) {
-//     const apiUrl = category
-//         ? `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=8545fe54b37e43e3bea9d5f9ba92764a`
-//         : `https://newsapi.org/v2/top-headlines?country=us&apiKey=8545fe54b37e43e3bea9d5f9ba92764a`
+export async function getAllArticles( category? : string) {
+    const apiUrl = category
+        ? `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=8545fe54b37e43e3bea9d5f9ba92764a`
+        : `https://newsapi.org/v2/top-headlines?country=us&apiKey=8545fe54b37e43e3bea9d5f9ba92764a`
 
-//     const res = await fetch(apiUrl)
-//     if(!res.ok) 
-//         throw {
-//             message: 'Failed to fetch articles.',
-//             statusText: res.statusText,
-//             status: res.status
-//         }
-//     const data = await res.json()
-//     console.log('category in api.ts:', category);
-//     return data
-// }
+    const res = await fetch(apiUrl)
+    if(!res.ok) 
+        throw {
+            message: 'Failed to fetch articles.',
+            statusText: res.statusText,
+            status: res.status
+        }
+    const data = await res.json()
+    console.log('category in api.ts:', category);
+    return data
+}
         
 //json placeholder version
 // export async function getAllArticles(category?: string) {
